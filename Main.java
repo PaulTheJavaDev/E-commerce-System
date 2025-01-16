b99647b487aaf5e3301c4b8ebb313d8d0e4523be
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-16 22:06:42 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //admin
+        Account PLS = new Account("PaulTheJavaDev", "HelloWorld!", 69420, true);
+
+        //guests
+        Account STF = new Account("StefanieJoy", "Sabrina2", 240, false);
+        Account PG = new Account("PeterGünstig", "Password1234", 100, false);
+
+
+
+    }
+
+}
