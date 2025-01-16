45f31faa63e1011ab1a4f70cb4eeefc4c5e32ce0
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-16 22:07:58 +0100

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product {
+
+    String name;
+    String description;
+    double price;
+    int stock;
+    String category;
+
+    Product(String name, String description, double price, int stock, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+}
